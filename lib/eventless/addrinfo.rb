78f37de3286aa1021e695f64920e503b29ea6cb3
Lucas Chi <chi.lucas@gmail.com>
2012-04-17 18:06:02 -0400

    implemented nearly all functions for addrinfo
    Two not implemented: #marshal_dump and #marshal_load.  Most methods are
    passed through (non-blocking), but some are blocking.  Blocking methods
    are either passed through because we already patched the underlying
    blocking calls or patched in addrinfo.

--- lib/eventless/addrinfo.rb
+++ lib/eventless/addrinfo.rb
@@ -17,9 +17,57 @@ module Eventless
       addrinfo
     end
 
-    # wrapped instance methods
-    [:afamily, :socktype, :protocol,
-     :to_sockaddr, :ip_address, :ip_port].each do |sym|
+    class << self
+      # wrapped blocking class methods that all need to
+      # have their return values cast to Eventless::Addrinfo
+      [:ip, :tcp, :udp].each do |sym|
+        define_method(sym) do |*args|
+          queue = Queue.new
+          watcher = Eventless.loop.async
+          Eventless.loop.attach(watcher)
+
+          Eventless.threadpool.schedule do
+            res = _wrap(RealAddrinfo.send(sym, *args))
+            queue << res
+            watcher.signal
+          end
+          Eventless.loop.transfer
+
+          queue.shift
+        end
+      end
+
+      # wrapped non-blocking class methods
+      [:unix].each do |sym|
+        define_method(sym) do |*args|
+          RealAddrinfo.send(sym, *args)
+        end
+      end
+    end
+
+    # wrapped blocking instance methods
+    [:ip_unpack, :inspect_sockaddr].each do |sym|
+      define_method(sym) do |*args|
+        RealAddrinfo.send(sym, *args)
+      end
+    end
+
+    # wrapped blocking instance methods that
+    # call ruby methods that have been patched
+    [:connect, :connect_from, :connect_to, :listen].each do |sym|
+      define_method(sym) do |*args|
+        @addrinfo.send(sym, *args)
+      end
+    end
+
+    # wrapped non-blocking instance methods
+    [:afamily, :socktype, :protocol, :to_sockaddr, :ip_address, :ip_port,
+     :bind, :canonname, :ip?, :ipv4?, :ipv4_loopback?, :ipv4_multicast?,
+     :ipv4_private?, :ipv6?, :ipv6_linklocal?, :ipv6_loopback?,
+     :ipv6_mc_global?, :ipv6_mc_linklocal?, :ipv6_mc_nodelocal?,
+     :ipv6_mc_orglocal?, :ipv6_mc_sitelocal?, :ipv6_multicast?,
+     :ipv6_sitelocal?, :ipv6_to_ipv4, :ipv6_unspecified?, :ipv6_v4compat?,
+     :ipv6_v4mapped?, :unix?, :to_s, :to_sockaddr, :pfamily, :unix_path].each do |sym|
       define_method(sym) do |*args|
         @addrinfo.send(sym, *args)
       end
@@ -36,6 +84,21 @@ module Eventless
 
       Eventless.threadpool.schedule do
         addrs = RealAddrinfo.getaddrinfo(*args).map { |ai| _wrap(ai) }
+        queue << addrs
+        watcher.signal
+      end
+      Eventless.loop.transfer
+
+      queue.shift
+    end
+
+    def family_addrinfo(*args)
+      queue = Queue.new
+      watcher = Eventless.loop.async
+      Eventless.loop.attach(watcher)
+
+      Eventless.threadpool.schedule do
+        addrs = RealAddrinfo.family_addrinfo(*args).map { |ai| _wrap(ai) }
         queue << addrs
         watcher.signal
       end
